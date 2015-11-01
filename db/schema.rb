f866bff52f242a42794b10823aed8f2ffff576dd
Dominique Miller <dominique.p.miller@gmail.com>
2015-11-01 18:54:39 -0500

    added date to events, styled photo section, events section(partial)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151031150121) do
+ActiveRecord::Schema.define(version: 20151101233040) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20151031150121) do
     t.integer  "zipcode"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "date"
   end
 
   create_table "photos", force: :cascade do |t|
