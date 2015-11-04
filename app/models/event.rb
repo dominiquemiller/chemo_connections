973d75faa22c1718e0d6c47c23accf9ea123355e
Dominique Miller <dominique.p.miller@gmail.com>
2015-11-04 14:32:36 -0500

    added search bar/styling/images

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,4 +5,7 @@ validates :address, presence: true
 validates :city, presence: true
 validates :state, presence: true
 validates :zipcode, presence: true
+
+scope :search, ->(keyword){ where('lower(city) LIKE ?', "%#{keyword}%") if keyword.present? }
+
 end
